114cd132cb5d8ee23bdaae6ba3732e54e715e4ef
Vaibhav Ghildiyal <vaibhavghildiyal2101@gmail.com>
2026-01-29 07:30:48 +0530

    feat: Initialize Next.js application with Clerk authentication, root layout, home page, and a resizable UI component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
 import { Button } from "@/components/ui/button";
-
+import {
+  SignUpButton,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 export default function Home() {
-  return <Button>Button</Button>;
+  return (
+    <>
+      <div>
+        <h1>DetWise-AI Powered Dental Assistant</h1>
+        <p>
+          AI-powered dental assistant that helps dentists diagnose and treat
+          dental problems.
+        </p>
+        <SignedOut>
+          <SignUpButton mode="modal">
+            <Button>Sign Up</Button>
+          </SignUpButton>
+          <SignInButton mode="modal">
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </>
+  );
 }
